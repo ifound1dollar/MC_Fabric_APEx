56b0ca249db36d9192bbb0753980392b0fd2296a
dollar <tmg1999@yahoo.com>
2024-03-23 19:26:14 -0500

    Add first versions of phosphate ore and cobalt shard, plus update some textures; add empty class for Battleaxe

---
+++ src/main/java/net/dollar/simplegear/item/custom/ModBattleaxeItem.java
@@ -0,0 +1,10 @@
+package net.dollar.simplegear.item.custom;
+
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+
+public class ModBattleaxeItem extends SwordItem {
+    public ModBattleaxeItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+}
