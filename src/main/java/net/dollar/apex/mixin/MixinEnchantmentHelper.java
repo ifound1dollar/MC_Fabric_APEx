6d91ea914f5a1e6827b10c61ac5afe00444c0a41
dollar <tmg1999@yahoo.com>
2024-10-12 15:52:48 -0500

    Update to version 1.2.1
    Fix broken enchantments for new Bows, Crossbows, and Armor items;

--- src/main/java/net/dollar/apex/mixin/MixinEnchantmentHelper.java
+++ src/main/java/net/dollar/apex/mixin/MixinEnchantmentHelper.java
@@ -1,10 +1,23 @@
 package net.dollar.apex.mixin;
 
 import net.dollar.apex.item.custom.ModBattleaxeItem;
+import net.dollar.apex.item.custom.ModBronzeArmorItem;
+import net.dollar.apex.item.custom.ModGildedBronzeArmorItem;
+import net.dollar.apex.item.custom.bow.ModCobaltSteelBowItem;
+import net.dollar.apex.item.custom.bow.ModInfusedGemstoneBowItem;
+import net.dollar.apex.item.custom.bow.ModTungstenCarbideBowItem;
+import net.dollar.apex.item.custom.cobaltsteel.ModCobaltSteelArmorItem;
+import net.dollar.apex.item.custom.crossbow.ModCobaltSteelCrossbowItem;
+import net.dollar.apex.item.custom.crossbow.ModInfusedGemstoneCrossbowItem;
+import net.dollar.apex.item.custom.crossbow.ModTungstenCarbideCrossbowItem;
+import net.dollar.apex.item.custom.infusedgemstone.ModInfusedGemstoneArmorItem;
+import net.dollar.apex.item.custom.tungstencarbide.ModTungstenCarbideArmorItem;
+import net.dollar.apex.util.MixinUtils;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,17 +44,9 @@ public class MixinEnchantmentHelper {
         //  be allowed to receive Mending at an Enchanting Table.
         List<EnchantmentLevelEntry> list = cir.getReturnValue();
 
-        //If the passed-in ItemStack is an Infused Gemstone item, add Mending as a list of valid enchantments.
-//        if (stack.getItem() instanceof IInfusedGemstoneItem) {
-//            //If within enchanting table power range, add mending as a possible enchantment to receive.
-//            if (power >= Enchantments.MENDING.getMinPower(1) && power <= Enchantments.MENDING.getMaxPower(1)) {
-//                list.add(new EnchantmentLevelEntry(Enchantments.MENDING, 1));   //Has to be level 1, apparently.
-//            }
-//        }
-
-        //If the passed-in ItemStack is a Battleaxe, add most weapon enchantments.
-        if (stack.getItem() instanceof ModBattleaxeItem) {
-            //Declare ArrayList of ONLY weapon enchantments, then iterate through each and add to pool.
+        Item item = stack.getItem();
+        if (item instanceof ModBattleaxeItem) {
+            //If the passed-in ItemStack is a Battleaxe, add most weapon enchantments.
             ArrayList<Enchantment> enchantments = new ArrayList<>() {
                 {
                     add(Enchantments.SHARPNESS);
@@ -52,6 +57,80 @@ public class MixinEnchantmentHelper {
                     add(Enchantments.LOOTING);
                 }
             };
+
+            block0: for (Enchantment enchantment : enchantments) {
+                //Iterate in range of all valid levels for this enchantment.
+                for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+                    //Skip if outside enchanting table power range, else add the enchantment then return to outer loop.
+                    if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) {
+                        continue;
+                    }
+
+                    list.add(new EnchantmentLevelEntry(enchantment, i));
+                    continue block0;  //Continue to OUTER block (adds only the highest possible enchantment level).
+                }
+            }
+        } else if (item instanceof ModCobaltSteelBowItem ||
+                item instanceof ModInfusedGemstoneBowItem ||
+                item instanceof ModTungstenCarbideBowItem) {
+            //If the passed-in ItemStack is a new Bow, add bow enchantments.
+            ArrayList<Enchantment> enchantments = new ArrayList<>() {
+                {
+                    add(Enchantments.FLAME);
+                    add(Enchantments.INFINITY);
+                    add(Enchantments.POWER);
+                    add(Enchantments.PUNCH);
+                    add(Enchantments.UNBREAKING);
+                }
+            };
+
+            block0:
+            for (Enchantment enchantment : enchantments) {
+                //Iterate in range of all valid levels for this enchantment.
+                for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+                    //Skip if outside enchanting table power range, else add the enchantment then return to outer loop.
+                    if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) {
+                        continue;
+                    }
+
+                    list.add(new EnchantmentLevelEntry(enchantment, i));
+                    continue block0;  //Continue to OUTER block (adds only the highest possible enchantment level).
+                }
+            }
+        } else if (item instanceof ModCobaltSteelCrossbowItem ||
+                item instanceof ModInfusedGemstoneCrossbowItem ||
+                item instanceof ModTungstenCarbideCrossbowItem) {
+            //If the passed-in ItemStack is a new Crossbow, add crossbow enchantments.
+            ArrayList<Enchantment> enchantments = new ArrayList<>() {
+                {
+                    add(Enchantments.MULTISHOT);
+                    add(Enchantments.PIERCING);
+                    add(Enchantments.QUICK_CHARGE);
+                    add(Enchantments.UNBREAKING);
+                }
+            };
+
+            block0: for (Enchantment enchantment : enchantments) {
+                //Iterate in range of all valid levels for this enchantment.
+                for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+                    //Skip if outside enchanting table power range, else add the enchantment then return to outer loop.
+                    if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) {
+                        continue;
+                    }
+
+                    list.add(new EnchantmentLevelEntry(enchantment, i));
+                    continue block0;  //Continue to OUTER block (adds only the highest possible enchantment level).
+                }
+            }
+        } else if (item instanceof ModBronzeArmorItem ||
+                item instanceof ModGildedBronzeArmorItem ||
+                item instanceof ModCobaltSteelArmorItem ||
+                item instanceof ModInfusedGemstoneArmorItem ||
+                item instanceof ModTungstenCarbideArmorItem) {
+            //If the passed-in ItemStack is a new Armor item, add armor enchantments.
+            //Retrieve list of enchantments for the armor type from helper method.
+            ArrayList<Enchantment> enchantments = MixinUtils.getEnchantmentsForArmorItem(item);
+
             block0: for (Enchantment enchantment : enchantments) {
                 //Iterate in range of all valid levels for this enchantment.
                 for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
