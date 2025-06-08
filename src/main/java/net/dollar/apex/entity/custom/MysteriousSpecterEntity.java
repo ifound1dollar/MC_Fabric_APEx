db4243b0a342402c5d3c9c20eee1f89c2049d721
Tanner Gaudes <tmg1999@yahoo.com>
2025-06-08 03:48:45 -0500

    More retroactive fixes for mod version 2.0.0
    Adjust spawn weights for Obsidian Golem and Mysterious Specter to 150 and 50, respectively. Reduce Mysterious Specter aura radius from 16 -> 10.

--- src/main/java/net/dollar/apex/entity/custom/MysteriousSpecterEntity.java
+++ src/main/java/net/dollar/apex/entity/custom/MysteriousSpecterEntity.java
@@ -285,7 +285,7 @@ public class MysteriousSpecterEntity extends HostileEntity implements Angerable 
      * Applies the Weakness and Hunger effect to all nearby Entities.
      */
     private void applyWeaknessHungerAura() {
-        double radius = 16.0;
+        double radius = 10.0;
         double x = this.getX();
         double y = this.getY();
         double z = this.getZ();
