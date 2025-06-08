aa0280d72b5a15e7631cdbe54e52774d68425caf
Tanner Gaudes <tmg1999@yahoo.com>
2025-06-08 02:31:29 -0500

    Minor retroactive fixes for mod version 2.0.0
    Fix duplicate Mysterious Specter models caused by missing Client-only environment annotation.

--- src/main/java/net/dollar/apex/entity/client/MysteriousSpecterModel.java
+++ src/main/java/net/dollar/apex/entity/client/MysteriousSpecterModel.java
@@ -1,11 +1,14 @@
 package net.dollar.apex.entity.client;
 
 import net.dollar.apex.entity.custom.MysteriousSpecterEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
 
+@Environment(EnvType.CLIENT)
 public class MysteriousSpecterModel<T extends MysteriousSpecterEntity> extends BipedEntityModel<T> {
     public MysteriousSpecterModel(ModelPart root) {
         super(root, RenderLayer::getEntityTranslucent);
