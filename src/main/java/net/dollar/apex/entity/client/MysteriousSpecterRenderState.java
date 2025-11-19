6f96446a2a52e599d7e49167576997c345abf0fa
Tanner Gaudes <tmg1999@yahoo.com>
2025-11-19 14:27:14 -0600

    Update mod to version 2.2.0
    Reduce Obsidian Golem and Mysterious Specter spawn rates by 33% each. Prevent new mobs from spawning in Mushroom Island biome. Fix new mobs not being removed from world in peaceful mode. Fix broken Mysterious Specter render issues by eliminating custom texture.

--- src/main/java/net/dollar/apex/entity/client/MysteriousSpecterRenderState.java
+++ src/main/java/net/dollar/apex/entity/client/MysteriousSpecterRenderState.java
@@ -3,15 +3,17 @@ package net.dollar.apex.entity.client;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.state.BipedEntityRenderState;
-import net.minecraft.util.math.random.Random;
 
 @Environment(EnvType.CLIENT)
 public class MysteriousSpecterRenderState extends BipedEntityRenderState {
     private final int textureID;
 
     public MysteriousSpecterRenderState() {
-        //Set textureID to a value between 0-4, which is used to determine which texture to render.
-        textureID = Random.create().nextInt(5);
+//        //Set textureID to a value between 0-4, which is used to determine which texture to render.
+//        textureID = Random.create().nextInt(5);
+
+        // Above was causing rapid swapping between textures for some reason, change to always 0.
+        this.textureID = 0;
     }
 
 
--- src/main/java/net/dollar/apex/entity/custom/MysteriousSpecterEntity.java
+++ src/main/java/net/dollar/apex/entity/custom/MysteriousSpecterEntity.java
@@ -32,6 +32,7 @@ import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.BiomeKeys;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -491,6 +492,9 @@ public class MysteriousSpecterEntity extends HostileEntity implements Angerable 
     public static boolean checkMysteriousSpecterSpawnRules(EntityType<MysteriousSpecterEntity> mysteriousSpecterEntityType,
                                                        ServerWorldAccess serverWorldAccess, SpawnReason spawnReason,
                                                        BlockPos blockPos, Random random) {
+        // Return false if biome at attempted spawn location is mushroom island.
+        if (serverWorldAccess.getBiome(blockPos).matchesKey(BiomeKeys.MUSHROOM_FIELDS)) return false;
+
         //Only allow spawn above a certain y-level (62 is sea level).
         if (blockPos.getY() < 62) {
             return false;
@@ -498,5 +502,14 @@ public class MysteriousSpecterEntity extends HostileEntity implements Angerable 
 
         //Calls the default mob spawn check, ignoring light levels entirely. Use canSpawnInDark instead.
         return canSpawnInDark(mysteriousSpecterEntityType, serverWorldAccess, spawnReason, blockPos, random);
+    }
+
+    /**
+     * Gets whether this mob should not exist in peaceful mode. Returns true here.
+     * @return Returns true if mob not allowed, false otherwise
+     */
+    @Override
+    protected boolean isDisallowedInPeaceful() {
+        return super.isDisallowedInPeaceful();
     }
 }
